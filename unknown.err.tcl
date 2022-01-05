0ae6f105ce0e3f08709b1020c0585013dfdc7ba5
igor20192 <igor.udovenko2012@meta.ua>
2022-01-05 11:39:22 +0200

    Добавьте файлы проекта.

---
+++ unknown.err.tcl
@@ -0,0 +1,18 @@
+can not find channel named "stderr"
+    while executing
+"puts stderr $result"
+    (procedure "vTcl:python_run" line 16)
+    invoked from within
+"vTcl:python_run $base $prefix"
+    (procedure "vTcl:python_quick_run" line 11)
+    invoked from within
+"vTcl:python_quick_run .vTcl.supp_console supp"
+    invoked from within
+".vTcl.supp_console.butframe.but34 invoke"
+    ("uplevel" body line 1)
+    invoked from within
+"uplevel #0 [list $w invoke]"
+    (procedure "tk::ButtonUp" line 24)
+    invoked from within
+"tk::ButtonUp .vTcl.supp_console.butframe.but34"
+    (command bound to event)
